3429c502eeb2aa15bc272fb72fcbcd606c1745cb
Alexander A. Guerra <alex@paper-rad.io>
2018-02-17 06:56:29 -0600

    Create subpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GlebaPageComponent } from './pages/silly/gleba-page/gleba-page.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'gleba', component: GlebaPageComponent }
+];
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule],
+    declarations: [AppComponent, HomeComponent, GlebaPageComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes)],
     providers: [],
     bootstrap: [AppComponent]
 })
---
+++ src/app/pages/silly/gleba-page/gleba-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+// import * as d3 from 'd3';
+
+@Component({
+    selector: 'app-gleba-page',
+    templateUrl: './gleba-page.component.html',
+    styleUrls: ['./gleba-page.component.css']
+})
+export class GlebaPageComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
