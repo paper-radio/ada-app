92f48a59a3d96f96dfeeb30edf9c0de756d54bb2
Alexander A. Guerra <alex@paper-rad.io>
2018-02-17 08:49:27 -0600

    Create new fun page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { GlebaPageComponent } from './pages/silly/gleba-page/gleba-page.component';
+import { EpiphanousPageComponent } from './pages/silly/epiphanous-page/epiphanous-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'gleba', component: GlebaPageComponent }
+    { path: 'gleba', component: GlebaPageComponent },
+    { path: 'Epiphanous', component: EpiphanousPageComponent }
 ];
 
 @NgModule({
-    declarations: [AppComponent, HomeComponent, GlebaPageComponent],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        GlebaPageComponent,
+        EpiphanousPageComponent
+    ],
     imports: [BrowserModule, RouterModule.forRoot(routes)],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/pages/silly/epiphanous-page/epiphanous-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-epiphanous-page',
+  templateUrl: './epiphanous-page.component.html',
+  styleUrls: ['./epiphanous-page.component.css']
+})
+export class EpiphanousPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
