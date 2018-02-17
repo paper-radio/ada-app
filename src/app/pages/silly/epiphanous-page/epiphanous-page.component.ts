c9939f34a3c6330e22b99c1054cee4295b5f1839
Alexander A. Guerra <alex@paper-rad.io>
2018-02-17 11:12:29 -0600

    Add ROT

--- src/app/pages/silly/epiphanous-page/epiphanous-page.component.ts
+++ src/app/pages/silly/epiphanous-page/epiphanous-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import * as ROT from 'rot-js';
 
 @Component({
-  selector: 'app-epiphanous-page',
-  templateUrl: './epiphanous-page.component.html',
-  styleUrls: ['./epiphanous-page.component.css']
+    selector: 'app-epiphanous-page',
+    templateUrl: './epiphanous-page.component.html',
+    styleUrls: ['./epiphanous-page.component.css']
 })
 export class EpiphanousPageComponent implements OnInit {
+    display: ROT.Display;
 
-  constructor() { }
+    constructor() {
+        this.display = new ROT.Display();
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        document.body.appendChild(this.display.getContainer());
+    }
 }
